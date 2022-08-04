7adb0c7c1f0360f17fae5c9598d4b453e3d88292
Guilherme Lisboa <guilherme.lisboa@paytec.com.br>
2022-08-04 08:33:26 -0300

    Splash sreen implementada

--- app/src/main/java/com/example/clone_instagram/main/view/MainActivity.kt
+++ app/src/main/java/com/example/clone_instagram/main/view/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsetsController
-import android.widget.Toolbar
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.WindowDecorActionBar
 import androidx.core.content.ContextCompat
 import com.example.clone_instagram.R
 
@@ -28,14 +26,11 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.main_toolbar)
         setSupportActionBar(toolbar)
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
-
-
     }
 }
--- app/src/main/java/com/example/clone_instagram/splash/view/SplashActivity.kt
+++ app/src/main/java/com/example/clone_instagram/splash/view/SplashActivity.kt
@@ -1,13 +1,22 @@
 package com.example.clone_instagram.splash.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 import com.example.clone_instagram.R
 import com.example.clone_instagram.common.base.DependencyInjector
+import com.example.clone_instagram.common.extension.animationEnd
 import com.example.clone_instagram.databinding.ActivitySplashBinding
+import com.example.clone_instagram.login.view.LoginActivity
+import com.example.clone_instagram.main.view.MainActivity
 import com.example.clone_instagram.splash.Splash
 import com.example.clone_instagram.splash.presentation.SplashPresenter
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity(), Splash.View {
 
     private lateinit var binding: ActivitySplashBinding
@@ -24,13 +33,54 @@ class SplashActivity : AppCompatActivity(), Splash.View {
 
         val repository = DependencyInjector.splashRepository()
         presenter = SplashPresenter(this,repository)
+
+        binding.splashImg.animate().apply {
+            setListener(animationEnd {
+                presenter.authenticated()
+            })
+            duration = 1000
+            alpha(1.0f)
+            start()
+        }
     }
 
     override fun goToMainScreen() {
+        binding.splashImg.animate().apply{
+            setListener(
+                animationEnd {
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                }
+            )
+            duration = 1000
+            startDelay = 1000
+            alpha(0.0f)
+            start()
+        }
 
     }
 
     override fun goToLoginScreen() {
+        binding.splashImg.animate().apply{
+            setListener(
+                animationEnd {
+                    val intent = Intent(baseContext, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                    overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+                }
+            )
+            duration = 1000
+            startDelay = 1000
+            alpha(0.0f)
+            start()
+        }
+    }
 
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
     }
 }
